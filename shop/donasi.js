ac06fb92431c8f3b3b5671300778b815f93b93b1
FathurStore07 <95979875+FathurStore07@users.noreply.github.com>
2021-12-13 17:52:35 +0900

    Add files via upload

---
+++ shop/donasi.js
@@ -0,0 +1,29 @@
+const donasibot = () => {
+return `DONASI BISA VIA BANK / E-MONEY
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 47326
+2. BCA 
+	ㅁ -
+3. BNI 
+	ㅁ -
+4. PERMATA
+	ㅁ -
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ 0823 3434 2055
+2. OVO
+	ㅁ 0823 3434 2055
+3. DANA
+	ㅁ 0823 3434 2055
+	
+Semuanya atas nama MU****UR
+`
+	}
+
+exports.donasibot = donasibot
+ 
